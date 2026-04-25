3845b1b23a5b16a2086195aa4f48b06f7d21246d
The Maanav <prettylioness0550@gmail.com>
2026-04-26 00:36:00 +0530

    ㅤ Аксиом Ботс ㅤ

--- AxiomMusic/utils/stream/stream.py
+++ AxiomMusic/utils/stream/stream.py
@@ -115,7 +115,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                img = await get_thumb(vidid, user)
+                img = await get_thumb(vidid, user_name)
                 button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -211,7 +211,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid, user)
+            img = await get_thumb(vidid, user_name)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -382,7 +382,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid, user)
+            img = await get_thumb(vidid, user_name)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
