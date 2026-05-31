fc46d489fb2775280c360b886a2b77b626670433
The Maanav <prettylioness0550@gmail.com>
2026-05-31 17:28:44 +0530

    Update queue.py

--- AxiomMusic/utils/inline/queue.py
+++ AxiomMusic/utils/inline/queue.py
@@ -75,14 +75,14 @@ def queue_back_markup(_, CPLAY):
 def aq_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="ʏᴛ-ᴀᴘɪ sᴛᴀᴛυs 📼", callback_data="api_status"),
+            InlineKeyboardButton(text="ʏᴛ-ᴀᴘɪ sᴛᴀᴛυs 📼", callback_data="api_status", style=ButtonStyle.PRIMARY),
         ],
         [
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
-            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}", style=ButtonStyle.DANGER),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}", style=ButtonStyle.DANGER),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}", style=ButtonStyle.DANGER), 
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}", style=ButtonStyle.DANGER), 
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}", style=ButtonStyle.DANGER),
         ],
         [InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close")]
     ]
