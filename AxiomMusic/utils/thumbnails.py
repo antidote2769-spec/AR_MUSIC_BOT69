1e749d90a06398441b0fd5fa24bd06cf6b47b9a5
antidote2769-spec <antidote2769@gmail.com>
2026-06-15 18:12:03 +0530

    Update thumbnails.py

--- AxiomMusic/utils/thumbnails.py
+++ AxiomMusic/utils/thumbnails.py
@@ -321,7 +321,7 @@ async def get_thumb(videoid: str) -> str:
         fill=NAME_COLOR,
         anchor="lm"
     )
-    draw.text((1255, 45), "Dev :- ☠︎︎𝙰𝚁_𝚄𝚗𝚔𝚗𝚘𝚠𝚗乂 ☆ ༆@𝓐𝓷☨𝓲𝓭𝓸𝓽𝓮𝟔𝟗▄︻̷ ┻ ═━一",                                          font=f_wm,  fill=TEXT_WHITE, anchor="rd")
+    draw.text((1255, 45), "Dev :- ☠︎︎𝙰𝚁_𝚄𝚗𝚔𝚗𝚘𝚠𝚗乂 ☆ ༆@𝓐𝓷☨𝓲𝓭𝓸𝓽𝓮𝟔𝟗▄︻̷ ┻ ═━一", font=f_wm,  fill=TEXT_WHITE, anchor="rd")
 
     base.save(output, "PNG", optimize=True)
 
